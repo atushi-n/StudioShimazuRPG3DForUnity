26874ed08de18d80c9233e20e7f9b6b1354a0ffc
atushi <nakanoatushi413@gmail.com>
2025-10-07 21:37:10 +0900

    プレイヤーの動きに合わせてアニメーションが連動するように対応「アイドル↔️歩行」

--- StudioShimazuRPG3DForUnity/Assets/Game/Scripts/PlayerManager.cs
+++ StudioShimazuRPG3DForUnity/Assets/Game/Scripts/PlayerManager.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    private Animator _animator;
+
     private float _horizontal;
     private float _vertical;
     private float _moveSpeed = 5f;
@@ -10,6 +12,7 @@ public class PlayerManager : MonoBehaviour
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -23,5 +26,6 @@ public class PlayerManager : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody.linearVelocity = new Vector3(_horizontal, 0, _vertical) * _moveSpeed;
+        _animator.SetFloat("MoveSpeed", _rigidbody.linearVelocity.magnitude);
     }
 }
