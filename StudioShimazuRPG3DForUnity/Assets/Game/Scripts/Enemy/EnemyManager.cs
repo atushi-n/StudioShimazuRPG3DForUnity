d7bd9b2cf0127caeb7405be88e7179fa58c33106
atushi <nakanoatushi413@gmail.com>
2025-10-09 21:37:57 +0900

    敵のアニメーション遷移を作成

--- StudioShimazuRPG3DForUnity/Assets/Game/Scripts/Enemy/EnemyManager.cs
+++ StudioShimazuRPG3DForUnity/Assets/Game/Scripts/Enemy/EnemyManager.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+
+/*
+Enemyのアニメーション: Playerとの距離に応じてアニメーションを切り替える
+- Idle: 遠い
+- Run: やや近い
+- Attack: 近い
+*/
+
+
 public class EnemyManager : MonoBehaviour
 {
     public Transform _target;
