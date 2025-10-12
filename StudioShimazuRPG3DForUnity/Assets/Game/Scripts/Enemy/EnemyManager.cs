fdc5f7c69c808994f88ce0d547bc892a09b0d09f
atushi <nakanoatushi413@gmail.com>
2025-10-12 10:39:39 +0900

    敵が攻撃しているときのみ斧の当たり判定が有効になるように対応

--- StudioShimazuRPG3DForUnity/Assets/Game/Scripts/Enemy/EnemyManager.cs
+++ StudioShimazuRPG3DForUnity/Assets/Game/Scripts/Enemy/EnemyManager.cs
@@ -14,18 +14,36 @@ public class EnemyManager : MonoBehaviour
     public Transform _target;
     private Animator _animator;
     private NavMeshAgent _navMeshAgent;
+    public Collider WeaponCollider;
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _navMeshAgent.destination = _target.position;
+        WeaponCollider.enabled = false;
     }
 
     private void Update()
     {
         _navMeshAgent.destination = _target.position;
         _animator.SetFloat("Distance", _navMeshAgent.remainingDistance);
+    }
+
+    /// <summary>
+    /// 武器の判定を無効にする
+    /// </summary>
+    public void HideColliderWeapon()
+    {
+        WeaponCollider.enabled = false;
+    }
+
+    /// <summary>
+    /// 武器の判定を有効にする
+    /// </summary>
+    public void ShowColliderWeapon()
+    {
+        WeaponCollider.enabled = true;
     }
 
     private void OnTriggerEnter(Collider other)
